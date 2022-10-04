8c7bac24cfddf4db7a120063af5677cb7fc61d83
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 17:39:28 +0300

    Check if N is honest

---
+++ CheckIfHonest/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Number N: ");
+string Number = Console.ReadLine();
+int N = int.Parse(Number);
+if(N % 2 == 0)
+{
+    Console.Write("Honest number");
+}
+else
+{
+    Console.Write("Odd number");
+}

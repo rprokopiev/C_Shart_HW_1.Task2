1404cb8d7c61fb676e8ce9da63161b88fd8e7ecb
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 17:22:34 +0300

    Max of 3 fin

---
+++ FindMaxOf3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter Number 1: ");
+string Number1 = Console.ReadLine();
+int N1 = int.Parse(Number1);
+Console.Write("Enter Number 2: ");
+string Number2 = Console.ReadLine();
+int N2 = int.Parse(Number2);
+Console.Write("Enter Number 3: ");
+string Number3 = Console.ReadLine();
+int N3 = int.Parse(Number3);
+int max = N1;
+if(N2>max) max=N2;
+if(N3>max) max=N3;
+Console.WriteLine(max);

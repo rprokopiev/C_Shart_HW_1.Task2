ab9259966df83eef253d58f46287800134eb9aa6
Renat Prokopiev <prokop4891@gmail.com>
2022-10-04 18:04:47 +0300

    Find Honest

---
+++ FindHonestNumb/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Number N: ");
+string Number = Console.ReadLine();
+int N = int.Parse(Number);
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    
+}
